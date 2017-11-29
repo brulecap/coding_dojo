53cf01658475d6a95d4d6e6b47fca7507d88d9d0
Bruce LeCaptain <brulecap@gmail.com>
2017-11-29 11:26:55 -0500

    Removing webfun files from Python repository

--- WebFundamentals/Javascript/users/jquery_functions.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$("#user_form").submit(function(event) {
-		console.log($("#user_form input[name=first_name]").val(), " herer");
-		event.preventDefault();
-		$("tbody").append("<tr><td>" + $("#user_form input[name=first_name]").val() + "</td><td>" + $("#user_form input[name=last_name]").val() + "</td><td>" + $("#user_form input[name=email_address]").val() + "</td><td>" + $("#user_form input[name=contact_number]").val() + "</td></tr>");
-	});
-})
